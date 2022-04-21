aad9b3137894198d7d53490bc6eac417ab917882
Oleksandr <kostryban1@gmail.com>
2022-04-21 19:00:03 +0300

    Create init-s3.sh
    added init script for creating s3 bucket

---
+++ week_2/init-s3.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo "Creating file example"
+touch testfile.txt
+echo "Creating AWS S3 Bucket"
+aws s3api create-bucket --bucket week-2-bucket --region us-east-1 --profile lohika
+echo "Uploading file to AWS S3 Bucket"
+aws s3api put-object --bucket week-2-bucket --key testfile.txt --body testfile.txt --profile lohika
