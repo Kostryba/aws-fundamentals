509a3ed55e140be363eb799fc688bdbf1b30a09b
Oleksandr <kostryban1@gmail.com>
2022-04-27 12:46:54 +0300

    Update ec2.tf
    used variables at ec2

--- week_2/ec2.tf
+++ week_2/ec2.tf
@@ -5,8 +5,8 @@ variable "instance_name" {
 }
 
 resource "aws_instance" "app_server" {
-  ami                    = "ami-03ededff12e34e59e"
-  instance_type          = "t2.micro"
+  ami                    = var.image_id
+  instance_type          = var.ec2_instance_type
   vpc_security_group_ids = [aws_security_group.http_and_ssh_group.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   user_data              = <<EOF
