b702414b56676874a38b08b2080060fc2cdc44ec
Oleksandr <kostryban1@gmail.com>
2022-05-11 11:11:23 +0300

    Add files via upload
    added ec2 nat

---
+++ ec2-nat.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "ec2-nat" {
+  ami                    = var.nat-image_id
+  instance_type          = var.ec2_instance_type
+  subnet_id              = aws_subnet.week4-subnet-public.id
+  vpc_security_group_ids = [aws_security_group.public-security-group.id]
+  source_dest_check      = false
+
+  user_data = <<EOF
+  		#! /bin/bash
+
+  	EOF
+
+  key_name = "okostryba_lohika_ssh"
+
+  tags = {
+    Name = "ec2-nat"
+  }
+}
+
