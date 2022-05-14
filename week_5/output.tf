f6f442b176dda72d30a3511d62b52d931b2a347d
Oleksandr <kostryban1@gmail.com>
2022-05-14 16:10:50 +0300

    fixed names

--- week_5/output.tf
+++ week_5/output.tf
@@ -2,10 +2,10 @@ output "app_server_public_ip" {
   value = element(aws_instance.app_server.*.public_ip, 0)
 }
 
-output "week_4_sns_topic_arn" {
-  value = aws_sns_topic.week_4_sns.arn
+output "week_5_sns_topic_arn" {
+  value = aws_sns_topic.week_5_sns.arn
 }
 
-output "week_4_sqs_queue_url" {
-  value = aws_sqs_queue.week_4_queue.url
+output "week_5_sqs_queue_url" {
+  value = aws_sqs_queue.week_5_queue.url
 }
